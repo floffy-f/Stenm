143639851231778e95640e89e0dd318310a36ca4
Vincent Florian <florianvincent@free.fr>
2021-07-29 15:54:53 +0200

    Planar-ps algorithms inserted in the elm project on the rust part, and linked with ports. Now we can see the png visualisation of the normal map WARNING : alot of warnings appear at compile time. This will be taken care of later.

--- stenm-wasm/src/lib.rs
+++ stenm-wasm/src/lib.rs
@@ -2,7 +2,7 @@
 
 use anyhow::{anyhow, Context};
 use image::DynamicImage;
-use nalgebra::{DMatrix, Vector6};
+use nalgebra::{DMatrix, Scalar, Vector6};
 use serde::Deserialize;
 use std::cell::RefCell;
 use std::io::Cursor;
@@ -10,17 +10,26 @@ use std::rc::Rc;
 use wasm_bindgen::prelude::*;
 
 use stenm::img::crop::{crop, recover_original_motion, Crop};
-use stenm::img::registration::{self, CanRegister};
 use stenm::interop::{IntoDMatrix, ToImage};
-use stenm::utils::CanEqualize;
 
 #[macro_use]
 mod utils; // define console_log! macro
+mod planar;
 
 #[wasm_bindgen(raw_module = "../worker.mjs")]
 extern "C" {
     #[wasm_bindgen(js_name = "shouldStop")]
     async fn should_stop(step: &str, progress: Option<u32>) -> JsValue; // bool
+}
+
+/// Configuration (parameters) of the registration algorithm.
+#[wasm_bindgen]
+#[derive(Debug, Clone, Copy, Deserialize)]
+pub struct Config {
+    pub max_iterations: usize,
+    pub threshold: f32,
+    pub verbosity: u32,
+    pub z_mean: f32,
 }
 
 // This wrapper trick is because we cannot have async functions referencing &self.
@@ -38,6 +47,9 @@ impl Stenm {
         let result = (*inner).borrow_mut().load(id, img_file);
         result
     }
+    pub fn push_light(&mut self, x: f32, y: f32, z: f32) {
+        self.0.borrow_mut().push_light(x, y, z)
+    }
     pub fn run(&mut self, params: JsValue) -> js_sys::Promise {
         let inner = Rc::clone(&self.0);
         wasm_bindgen_futures::future_to_promise(async_run_rc(inner, params))
@@ -45,11 +57,14 @@ impl Stenm {
     pub fn image_ids(&self) -> Result<JsValue, JsValue> {
         self.0.borrow().image_ids()
     }
-    pub fn cropped_img_file(&self, i: usize) -> Result<Box<[u8]>, JsValue> {
-        self.0.borrow().cropped_img_file(i)
+    pub fn normal_map(&self) -> Result<Box<[u8]>, JsValue> {
+        self.0.borrow().normal_map()
     }
-    pub fn register_and_save(&self, i: usize) -> Result<Box<[u8]>, JsValue> {
-        self.0.borrow().register_and_save(i)
+    // pub fn cropped_img_file(&self, i: usize) -> Result<Box<[u8]>, JsValue> {
+    //     self.0.borrow().cropped_img_file(i)
+    // }
+    pub fn register_and_save(&self) -> Result<Box<[u8]>, JsValue> {
+        self.0.borrow().register_and_save()
     }
 }
 
@@ -65,8 +80,9 @@ async fn async_run_rc(
 struct StenmInner {
     image_ids: Vec<String>,
     dataset: Dataset,
-    crop_registered: Vec<DMatrix<u8>>,
-    motion_vec: Option<Vec<Vector6<f32>>>,
+    // motion_vec: Option<Vec<Vector6<f32>>>,
+    lights: Vec<(f32, f32, f32)>,
+    normal_map: Vec<u8>,
 }
 
 enum Dataset {
@@ -81,9 +97,18 @@ enum Dataset {
 #[derive(Deserialize)]
 /// Type holding the algorithm parameters
 pub struct Args {
-    pub config: registration::Config,
-    pub equalize: Option<f32>,
+    pub config: Config,
     pub crop: Option<Crop>,
+}
+
+/// 3D point, representing a 3D light direction
+#[cfg_attr(feature = "wasm_bindgen", wasm_bindgen)]
+#[derive(Debug, Clone, Copy)]
+#[cfg_attr(feature = "serde", derive(Deserialize))]
+pub struct Point3d {
+    pub x: f32,
+    pub y: f32,
+    pub z: f32,
 }
 
 impl StenmInner {
@@ -94,9 +119,14 @@ impl StenmInner {
         Self {
             image_ids: Vec::new(),
             dataset: Dataset::Empty,
-            crop_registered: Vec::new(),
-            motion_vec: None,
+            // motion_vec: None,
+            lights: Vec::new(),
+            normal_map: Vec::<u8>::new(),
         }
+    }
+
+    pub fn push_light(&mut self, x: f32, y: f32, z: f32) {
+        self.lights.push((x, y, z));
     }
 
     // Load and decode the images to be registered.
@@ -176,82 +206,100 @@ impl StenmInner {
     // Run the main stenm registration algorithm.
     //                                                 Vec<f32>
     async fn run(&mut self, params: JsValue) -> Result<JsValue, JsValue> {
-        self.motion_vec = None;
-        self.crop_registered.clear();
+        // self.motion_vec = None;
+        self.normal_map.clear();
         let args: Args = params.into_serde().unwrap();
         utils::WasmLogger::setup(utils::verbosity_filter(args.config.verbosity));
 
-        // Use the algorithm corresponding to the type of data.
-        let motion_vec = match &self.dataset {
-            Dataset::Empty => Vec::new(),
+        log::info!("No conf yet");
+
+        let conf: planar::pps::Config_pps = planar::pps::Config_pps {
+            max_iterations: args.config.max_iterations,
+            threshold: args.config.threshold,
+            z_mean: args.config.z_mean,
+            lights: self.lights.clone(),
+        };
+
+        log::info!("Conf : {}", conf.z_mean);
+
+        // // Use the algorithm corresponding to the type of data.
+        let (_, normals, _) = match &self.dataset {
+            Dataset::Empty => todo!(), //Vec::new(),
             Dataset::GrayImages(gray_imgs) => {
-                let (motion_vec_crop, cropped_eq_imgs) =
-                    crop_and_register(&args, gray_imgs.clone(), 40)
-                        .await
-                        .map_err(utils::report_error)?;
-                log::info!("Applying registration on cropped images ...");
-                self.crop_registered = registration::reproject_may_stop::<u8, f32, u8, _>(
-                    &cropped_eq_imgs,
-                    &motion_vec_crop,
-                    should_stop_bool,
-                )
-                .await
-                .map_err(utils::report_error)?;
-                original_motion(args.crop, motion_vec_crop)
+                todo!()
             }
             Dataset::GrayImagesU16(gray_imgs) => {
-                let (motion_vec_crop, cropped_eq_imgs) =
-                    crop_and_register(&args, gray_imgs.clone(), 10 * 256)
-                        .await
-                        .map_err(utils::report_error)?;
-                log::info!("Applying registration on cropped images ...");
-                let cropped_u8: Vec<_> = cropped_eq_imgs.into_iter().map(into_gray_u8).collect();
-                self.crop_registered = registration::reproject_may_stop::<u8, f32, u8, _>(
-                    &cropped_u8,
-                    &motion_vec_crop,
-                    should_stop_bool,
-                )
-                .await
-                .map_err(utils::report_error)?;
-                original_motion(args.crop, motion_vec_crop)
+                todo!()
             }
             Dataset::RgbImages(imgs) => {
-                let gray_imgs: Vec<_> = imgs.iter().map(|im| im.map(|(_r, g, _b)| g)).collect();
-                let (motion_vec_crop, cropped_eq_imgs) = crop_and_register(&args, gray_imgs, 40)
-                    .await
+                // let cropped_imgs: &Vec<DMatrix<(u8, u8, u8)>> = match args.crop {
+                //     None => imgs,
+                //     Some(frame) => imgs
+                //         .iter()
+                //         .map(|im| {
+                //             im.slice(
+                //                 (frame.top, frame.left),
+                //                 (frame.bottom - frame.top, frame.right - frame.left),
+                //             )
+                //             .into_owned()
+                //         })
+                //         .collect(),
+                // };
+                let raw_images: Vec<DMatrix<f32>> = match args.crop {
+                    None => imgs.iter().map(|im| f32_image_matrix(im)).collect(),
+                    Some(frame) => {
+                        let cropped: Result<Vec<DMatrix<f32>>, _> = imgs
+                            .iter()
+                            .map(|im| {
+                                log::info!("1st px : {:?}", im[1]);
+                                im
+                            })
+                            .map(|im| f32_image_matrix(im))
+                            .map(|im| {
+                                // let mean = im.as_ref().map(|i| i.mean()).unwrap_or(0.0);
+                                log::info!("1st float : {:?}", im[1]);
+                                let mean = im.mean();
+                                log::info!("Moy :{}", mean);
+                                im
+                            })
+                            .map(|im| crop(frame, &im))
+                            .collect();
+                        match cropped {
+                            Err(e) => {
+                                log::info!("Problem while cropping : {}", e);
+                                imgs.iter().map(|im| f32_image_matrix(im)).collect()
+                            }
+                            Ok(cr) => {
+                                log::info!("No problem while cropping");
+                                cr
+                            }
+                        }
+                    }
+                };
+                // let raw_images: Vec<DMatrix<f32>> =
+                //     raw_images.map_err(utils::report_error).context("Pbm crop");
+                log::info!("RGB");
+                let results = planar::pps::photometric_stereo(conf, &raw_images)
                     .map_err(utils::report_error)?;
-                log::info!("Applying registration on cropped images ...");
-                self.crop_registered = registration::reproject_may_stop::<u8, f32, u8, _>(
-                    &cropped_eq_imgs,
-                    &motion_vec_crop,
-                    should_stop_bool,
-                )
-                .await
-                .map_err(utils::report_error)?;
-                original_motion(args.crop, motion_vec_crop)
+                log::info!("Planar ok");
+                results
             }
             Dataset::RgbImagesU16(imgs) => {
-                let gray_imgs: Vec<_> = imgs.iter().map(|im| im.map(|(_r, g, _b)| g)).collect();
-                let (motion_vec_crop, cropped_eq_imgs) =
-                    crop_and_register(&args, gray_imgs, 10 * 256)
-                        .await
-                        .map_err(utils::report_error)?;
-                log::info!("Applying registration on cropped images ...");
-                let cropped_u8: Vec<_> = cropped_eq_imgs.into_iter().map(into_gray_u8).collect();
-                self.crop_registered = registration::reproject_may_stop::<u8, f32, u8, _>(
-                    &cropped_u8,
-                    &motion_vec_crop,
-                    should_stop_bool,
-                )
-                .await
-                .map_err(utils::report_error)?;
-                original_motion(args.crop, motion_vec_crop)
+                todo!()
             }
         };
 
-        let flat_motion_vec: Vec<f32> = motion_vec.iter().flatten().cloned().collect();
-        self.motion_vec = Some(motion_vec);
-        JsValue::from_serde(&flat_motion_vec).map_err(utils::report_error)
+        log::info!("Computations ok");
+
+        self.normal_map = planar::main::save_normals(&normals).map_err(utils::report_error)?;
+
+        log::info!("Encode PNG OK");
+
+        let false_vec: Vec<f32> = Vec::new();
+        JsValue::from_serde(&false_vec).map_err(utils::report_error)
+        // let flat_motion_vec: Vec<f32> = motion_vec.iter().flatten().cloned().collect();
+        // self.motion_vec = Some(motion_vec);
+        // JsValue::from_serde(&flat_motion_vec).map_err(utils::report_error)
     }
 
     // Return the ids of loaded images: [string]
@@ -259,42 +307,49 @@ impl StenmInner {
         JsValue::from_serde(&self.image_ids).map_err(utils::report_error)
     }
 
-    // Retrieve the cropped registered images.
-    pub fn cropped_img_file(&self, i: usize) -> Result<Box<[u8]>, JsValue> {
-        encode(i, &self.crop_registered[i]).map_err(utils::report_error)
+    pub fn normal_map(&self) -> Result<Box<[u8]>, JsValue> {
+        // WARNING : is the clone useful ??
+        // Maybe we don't need to lose ownership.
+        Ok(self.normal_map.clone().into_boxed_slice())
     }
+    // // Retrieve the cropped registered images.
+    // pub fn cropped_img_file(&self, i: usize) -> Result<Box<[u8]>, JsValue> {
+    //     encode(i, &self.crop_registered[i]).map_err(utils::report_error)
+    // }
 
     // Register and save that image.
-    pub fn register_and_save(&self, i: usize) -> Result<Box<[u8]>, JsValue> {
-        log::info!("Registering image {}", i);
-        match (&self.motion_vec, &self.dataset) {
-            (_, Dataset::Empty) => {
-                Err(anyhow!("Images not loaded yet")).map_err(utils::report_error)
-            }
-            (None, _) => {
-                Err(anyhow!("Registration parameters unknown")).map_err(utils::report_error)
-            }
-            (Some(all_motion), Dataset::GrayImages(images)) => {
-                let registered: DMatrix<u8> =
-                    stenm::img::registration::warp(&images[i], &all_motion[i]);
-                encode(i, &registered).map_err(utils::report_error)
-            }
-            (Some(all_motion), Dataset::GrayImagesU16(images)) => {
-                let registered: DMatrix<u16> =
-                    stenm::img::registration::warp(&images[i], &all_motion[i]);
-                encode(i, &registered).map_err(utils::report_error)
-            }
-            (Some(all_motion), Dataset::RgbImages(images)) => {
-                let registered: DMatrix<(u8, u8, u8)> =
-                    stenm::img::registration::warp(&images[i], &all_motion[i]);
-                encode(i, &registered).map_err(utils::report_error)
-            }
-            (Some(all_motion), Dataset::RgbImagesU16(images)) => {
-                let registered: DMatrix<(u16, u16, u16)> =
-                    stenm::img::registration::warp(&images[i], &all_motion[i]);
-                encode(i, &registered).map_err(utils::report_error)
-            }
-        }
+    pub fn register_and_save(&self) -> Result<Box<[u8]>, JsValue> {
+        log::info!("Downloading normal map as PNG");
+        // Err(anyhow!("Images not loaded yet")).map_err(utils::report_error)
+        Ok(self.normal_map.clone().into_boxed_slice())
+        // match (&self.motion_vec, &self.dataset) {
+        //     (_, Dataset::Empty) => {
+        //         Err(anyhow!("Images not loaded yet")).map_err(utils::report_error)
+        //     }
+        //     (None, _) => {
+        //         Err(anyhow!("Registration parameters unknown")).map_err(utils::report_error)
+        //     }
+        //     (Some(all_motion), Dataset::GrayImages(images)) => {
+        //         let registered: DMatrix<u8> =
+        //             stenm::img::registration::warp(&images[i], &all_motion[i]);
+        //         encode(i, &registered).map_err(utils::report_error)
+        //     }
+        //     (Some(all_motion), Dataset::GrayImagesU16(images)) => {
+        //         let registered: DMatrix<u16> =
+        //             stenm::img::registration::warp(&images[i], &all_motion[i]);
+        //         encode(i, &registered).map_err(utils::report_error)
+        //     }
+        //     (Some(all_motion), Dataset::RgbImages(images)) => {
+        //         let registered: DMatrix<(u8, u8, u8)> =
+        //             stenm::img::registration::warp(&images[i], &all_motion[i]);
+        //         encode(i, &registered).map_err(utils::report_error)
+        //     }
+        //     (Some(all_motion), Dataset::RgbImagesU16(images)) => {
+        //         let registered: DMatrix<(u16, u16, u16)> =
+        //             stenm::img::registration::warp(&images[i], &all_motion[i]);
+        //         encode(i, &registered).map_err(utils::report_error)
+        //     }
+        // }
     }
 }
 
@@ -306,42 +361,42 @@ fn encode<Im: ToImage>(i: usize, mat: &Im) -> anyhow::Result<Box<[u8]>> {
     Ok(buffer.into_boxed_slice())
 }
 
-#[allow(clippy::type_complexity)]
-async fn crop_and_register<T: CanEqualize + CanRegister>(
-    args: &Args,
-    gray_imgs: Vec<DMatrix<T>>,
-    sparse_diff_threshold: <T as CanRegister>::Bigger,
-) -> anyhow::Result<(Vec<Vector6<f32>>, Vec<DMatrix<T>>)>
-where
-    DMatrix<T>: ToImage,
-{
-    // Extract the cropped area from the images.
-    let cropped_imgs: Result<Vec<DMatrix<T>>, _> = match args.crop {
-        None => Ok(gray_imgs),
-        Some(frame) => {
-            log::info!("Cropping images ...");
-            gray_imgs.iter().map(|im| crop(frame, im)).collect()
-        }
-    };
-    let mut cropped_imgs = cropped_imgs.context("Failed to crop images")?;
-
-    // Equalize mean intensities of cropped area.
-    if let Some(mean_intensity) = args.equalize {
-        log::info!("Equalizing images mean intensities ...");
-        stenm::utils::equalize_mean(mean_intensity, &mut cropped_imgs);
-    }
-
-    // Compute the motion of each image for registration.
-    log::info!("Registration of images ...");
-    registration::async_gray_affine(
-        args.config,
-        cropped_imgs,
-        sparse_diff_threshold,
-        should_stop_bool,
-    )
-    .await
-    .context("Failed to register images")
-}
+// #[allow(clippy::type_complexity)]
+// async fn crop_and_register<T: CanEqualize + CanRegister>(
+//     args: &Args,
+//     gray_imgs: Vec<DMatrix<T>>,
+//     sparse_diff_threshold: <T as CanRegister>::Bigger,
+// ) -> anyhow::Result<(Vec<Vector6<f32>>, Vec<DMatrix<T>>)>
+// where
+//     DMatrix<T>: ToImage,
+// {
+//     // Extract the cropped area from the images.
+//     let cropped_imgs: Result<Vec<DMatrix<T>>, _> = match args.crop {
+//         None => Ok(gray_imgs),
+//         Some(frame) => {
+//             log::info!("Cropping images ...");
+//             gray_imgs.iter().map(|im| crop(frame, im)).collect()
+//         }
+//     };
+//     let mut cropped_imgs = cropped_imgs.context("Failed to crop images")?;
+//
+//     // Equalize mean intensities of cropped area.
+//     if let Some(mean_intensity) = args.equalize {
+//         log::info!("Equalizing images mean intensities ...");
+//         stenm::utils::equalize_mean(mean_intensity, &mut cropped_imgs);
+//     }
+//
+//     // Compute the motion of each image for registration.
+//     log::info!("Registration of images ...");
+//     registration::async_gray_affine(
+//         args.config,
+//         cropped_imgs,
+//         sparse_diff_threshold,
+//         should_stop_bool,
+//     )
+//     .await
+//     .context("Failed to register images")
+// }
 
 async fn should_stop_bool(step: &str, progress: Option<u32>) -> bool {
     let js_bool = should_stop(step, progress).await;
@@ -358,4 +413,27 @@ fn original_motion(crop: Option<Crop>, motion_vec_crop: Vec<Vector6<f32>>) -> Ve
 
 fn into_gray_u8(m: DMatrix<u16>) -> DMatrix<u8> {
     m.map(|x| (x / 256) as u8)
+}
+
+trait IntoF32Gray {
+    fn into_gray_f32(self) -> f32;
+}
+
+impl IntoF32Gray for (u8, u8, u8) {
+    fn into_gray_f32(self) -> f32 {
+        (0.2989 * self.0 as f32 + 0.5870 * self.1 as f32 + 0.1140 * self.2 as f32).min(255.0)
+            / 255.0
+    }
+}
+impl IntoF32Gray for (u16, u16, u16) {
+    fn into_gray_f32(self) -> f32 {
+        (0.2989 * self.0 as f32 + 0.5870 * self.1 as f32 + 0.1140 * self.2 as f32)
+            .min(256.0 * 256.0 - 1.0)
+            / (256.0 * 256.0 - 1.0)
+    }
+}
+
+fn f32_image_matrix<T: IntoF32Gray + Clone + Scalar>(mat: &DMatrix<T>) -> DMatrix<f32> {
+    let (d1, d2) = mat.shape();
+    DMatrix::from_iterator(d1, d2, mat.iter().map(|pix| pix.clone().into_gray_f32()))
 }
